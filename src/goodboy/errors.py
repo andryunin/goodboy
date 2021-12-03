5374fdc067f38724e444c30f9b6cb80ff4d67bae
Maxim Andryunin <maxim.andryunin@gmail.com>
2021-12-03 19:37:55 +0700

    i18n: translate error messages

--- src/goodboy/errors.py
+++ src/goodboy/errors.py
@@ -64,8 +64,33 @@ class MessageCollection:
 
 DEFAULT_MESSAGES = MessageCollection(
     {
-        "cannot_be_none": Message(_("Cannot be None"), json=_("Cannot be null")),
-        "unexpected_type": Message(_("Should be '{type}' type")),
+        # Common messages
+        "cannot_be_none": Message(_("cannot be None"), json=_("cannot be null")),
+        "cannot_be_blank": Message(_("cannot be blank")),
+        "unexpected_type": Message(_('expected type is "{type}"')),
+        # Date/DateTime messages
+        "earlier_or_equal_to": Message(_("must be later than {value}")),
+        "earlier_than": Message(_("must be later or equal to {value}")),
+        "later_or_equal_to": Message(_("must be earlier than {value}")),
+        "later_than": Message(_("must be earlier or equal to {value}")),
+        "invalid_date_format": Message(_("invalid date format")),
+        "invalid_datetime_format": Message(_("invalid date and time format")),
+        # Numeric messages
+        "greater_or_equal_to": Message(_("must be less than {value}")),
+        "greater_than": Message(_("must be less or equal to {value}")),
+        "less_or_equal_to": Message(_("must be greater than {value}")),
+        "less_than": Message(_("must be greater or equal to {value}")),
+        "invalid_integer_format": Message(_("invalid integer format")),
+        "invalid_numeric_format": Message(_("invalid numeric format")),
+        # String messages
+        "invalid_string_format": Message(_("invalid string format")),
+        "invalid_string_length": Message(_("must be {value} characters long")),
+        "string_too_long": Message(_("must be shorter than {value} characters")),
+        "string_too_short": Message(_("must be longer than {value} characters")),
+        # Dict messages
+        "keys_error": Message(_("key errors")),
+        "required_key": Message(_("key is required")),
+        "unknown_key": Message(_("unknown key")),
     }
 )
 
