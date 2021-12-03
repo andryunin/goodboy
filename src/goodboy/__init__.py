3ba2e8241fec01a579566ee5ef0e71f2daebc339
Maxim Andryunin <maxim.andryunin@gmail.com>
2021-12-03 22:51:17 +0700

    feat: expose public interface through root module

--- src/goodboy/__init__.py
+++ src/goodboy/__init__.py
@@ -1,1 +1,21 @@
+from goodboy.types.dates import Date, DateTime
+from goodboy.types.dicts import Dict, Key
+from goodboy.types.numeric import Float, Int
+from goodboy.types.simple import AnyType, Str
+from goodboy.validator import Result, Validator, validate
+
 __version__ = "0.1.0"
+
+__all__ = [
+    "AnyType",
+    "Date",
+    "DateTime",
+    "Dict",
+    "Float",
+    "Int",
+    "Key",
+    "Result",
+    "Str",
+    "validate",
+    "Validator",
+]
