b02abcdd4954f567051d149f2a11f64967e21433
Maxim Andryunin <maxim.andryunin@gmail.com>
2021-12-03 17:14:31 +0700

    feat: add options to int validation

--- src/goodboy/types.py
+++ src/goodboy/types.py
@@ -78,38 +78,48 @@ class Int(Schema):
         *,
         allow_none: bool = False,
         messages: MessageCollection = DEFAULT_MESSAGES,
-        min: Optional[int] = None,
-        max: Optional[int] = None,
+        less_than: Optional[int] = None,
+        less_or_equal_to: Optional[int] = None,
+        greater_than: Optional[int] = None,
+        greater_or_equal_to: Optional[int] = None,
     ):
         super().__init__(allow_none=allow_none, messages=messages)
-        self.min = min
-        self.max = max
+        self.less_than = less_than
+        self.less_or_equal_to = less_or_equal_to
+        self.greater_than = greater_than
+        self.greater_or_equal_to = greater_or_equal_to
 
     def validate(self, value, typecast):
         if not isinstance(value, int):
-            return None, [Error("invalid_type", {"expected_type": "int"})]
+            return None, [self.error("unexpected_type", {"expected_type": "integer"})]
 
         errors = []
 
-        if self.min is not None and value < self.min:
-            errors.append(Error("int.less_then", {"min": self.min}))
+        if self.less_than is not None and value >= self.less_than:
+            errors.append(self.error("greater_or_equal_to", {"value": self.less_than}))
 
-        if self.max is not None and value >= self.max:
-            errors.append(Error("int.more_or_equal_then", {"max": self.max}))
+        if self.less_or_equal_to is not None and value > self.less_or_equal_to:
+            errors.append(self.error("greater_than", {"value": self.less_or_equal_to}))
+
+        if self.greater_than is not None and value <= self.greater_than:
+            errors.append(self.error("less_or_equal_to", {"value": self.greater_than}))
+
+        if self.greater_or_equal_to is not None and value < self.greater_or_equal_to:
+            errors.append(self.error("less_than", {"value": self.greater_or_equal_to}))
 
         return value, errors
 
-    def typecast(self, value):
-        if isinstance(value, int):
-            return value, []
+    def typecast(self, input):
+        if isinstance(input, int):
+            return input, []
 
-        if not isinstance(value, str):
-            return None, [Error("int.invalid_type_to_cast")]
+        if not isinstance(input, str):
+            return None, [self.error("unexpected_type", {"expected_type": "integer"})]
 
         try:
-            return int(value), []
+            return int(input), []
         except ValueError:
-            return None, [Error("int.invalid_format")]
+            return None, [self.error("invalid_integer_format")]
 
 
 class Str(Schema):
--- tests/types/test_int.py
+++ tests/types/test_int.py
@@ -1,50 +1,110 @@
+import pytest
+
 from goodboy.errors import Error
 from goodboy.types import Int
 from tests.types.conftest import assert_errors
 
 
-def test_min():
-    minimum = 0
-    schema = Int(min=minimum)
-
-    assert schema(minimum) == minimum
-    assert schema(minimum + 1) == minimum + 1
-
-    with assert_errors([Error("int.less_then", {"min": minimum})]):
-        schema(minimum - 1)
+def test_accepts_none_when_none_allowed():
+    schema = Int(allow_none=True)
+    assert schema(None) is None
 
 
-def test_max():
-    maximum = 0
-    schema = Int(max=0)
-
-    assert schema(maximum - 1) == maximum - 1
-
-    with assert_errors([Error("int.more_or_equal_then", {"max": maximum})]):
-        schema(maximum + 1)
-
-    with assert_errors([Error("int.more_or_equal_then", {"max": maximum})]):
-        schema(maximum)
-
-
-def test_typecheck():
+def test_rejects_none_when_none_denied():
     schema = Int()
 
-    assert schema(42) == 42
-
-    with assert_errors([Error("invalid_type", {"expected_type": "int"})]):
-        schema("42")
-
-
-def test_typecast():
-    assert Int()("42", typecast=True) == 42
-
-    with assert_errors([Error("int.invalid_format")]):
-        Int()("oops", typecast=True)
-
-
-def test_allow_none():
-    assert Int(allow_none=True)(None) is None
-
     with assert_errors([Error("cannot_be_none")]):
-        Int()(None)
+        schema(None)
+
+
+def test_accepts_datetime_type():
+    schema = Int()
+    good_value = 42
+
+    assert schema(good_value) == good_value
+
+
+def test_rejects_non_datetime_type():
+    schema = Int()
+    bad_value = "42"
+
+    with assert_errors([Error("unexpected_type", {"expected_type": "integer"})]):
+        schema(bad_value)
+
+
+def test_type_casting_accepts_good_input():
+    schema = Int()
+    good_input = "42"
+    value = 42
+
+    assert schema(good_input, typecast=True) == value
+
+
+def test_type_casting_rejects_bad_input():
+    schema = Int()
+    bad_input = "42.0"
+
+    with assert_errors([Error("invalid_integer_format")]):
+        schema(bad_input, typecast=True)
+
+
+def test_less_than_option_accepts_good_value():
+    schema = Int(less_than=0)
+    good_value = -1
+
+    assert schema(good_value) == good_value
+
+
+@pytest.mark.parametrize("bad_value", [0, 1])
+def test_less_than_option_rejects_bad_values(bad_value):
+    less_than_value = 0
+    schema = Int(less_than=less_than_value)
+
+    with assert_errors([Error("greater_or_equal_to", {"value": less_than_value})]):
+        schema(bad_value)
+
+
+@pytest.mark.parametrize("good_value", [0, -1])
+def test_less_or_equal_to_option_accepts_good_values(good_value):
+    schema = Int(less_or_equal_to=0)
+    assert schema(good_value) == good_value
+
+
+def test_less_or_equal_to_option_rejects_bad_value():
+    less_or_equal_to_value = 0
+    schema = Int(less_or_equal_to=less_or_equal_to_value)
+    bad_value = 1
+
+    with assert_errors([Error("greater_than", {"value": less_or_equal_to_value})]):
+        schema(bad_value)
+
+
+def test_greater_than_option_accepts_good_value():
+    schema = Int(greater_than=0)
+    good_value = 1
+
+    assert schema(good_value) == good_value
+
+
+@pytest.mark.parametrize("bad_value", [0, -1])
+def test_greater_than_option_rejects_bad_values(bad_value):
+    greater_than_value = 0
+    schema = Int(greater_than=greater_than_value)
+
+    with assert_errors([Error("less_or_equal_to", {"value": greater_than_value})]):
+        schema(bad_value)
+
+
+@pytest.mark.parametrize("good_value", [0, 1])
+def test_greater_or_equal_to_option_accepts_good_values(good_value):
+    schema = Int(greater_or_equal_to=0)
+    assert schema(good_value) == good_value
+
+
+def test_greater_or_equal_to_option_rejects_bad_value():
+    greater_or_equal_to_value = 0
+    schema = Int(greater_or_equal_to=0)
+    bad_value = -1
+
+    with assert_errors([Error("less_than", {"value": greater_or_equal_to_value})]):
+        schema(bad_value)
