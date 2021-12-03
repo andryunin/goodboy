c941d7cdd708a4fe98eca065c9a153f0382f99cd
Maxim Andryunin <maxim.andryunin@gmail.com>
2021-12-03 16:55:32 +0700

    feat: add options for datetime validation

--- src/goodboy/types.py
+++ src/goodboy/types.py
@@ -22,44 +22,54 @@ class DateTime(Schema):
         *,
         allow_none: bool = False,
         messages: MessageCollection = DEFAULT_MESSAGES,
-        min: Optional[datetime] = None,
-        max: Optional[datetime] = None,
+        earlier_than: Optional[datetime] = None,
+        earlier_or_equal_to: Optional[datetime] = None,
+        later_than: Optional[datetime] = None,
+        later_or_equal_to: Optional[datetime] = None,
         format: str = None,
     ):
         super().__init__(allow_none=allow_none, messages=messages)
-        self.min = min
-        self.max = max
+        self.earlier_than = earlier_than
+        self.earlier_or_equal_to = earlier_or_equal_to
+        self.later_than = later_than
+        self.later_or_equal_to = later_or_equal_to
         self.format = format
 
     def validate(self, value, typecast):
         if not isinstance(value, datetime):
-            return None, [Error("invalid_type", {"expected_type": "datetime"})]
+            return None, [self.error("unexpected_type", {"expected_type": "datetime"})]
 
         errors = []
 
-        if self.min and value < self.min:
-            errors.append(Error("datetime.less_then", {"min": self.min}))
+        if self.earlier_than and value >= self.earlier_than:
+            errors.append(self.error("later_or_equal_to", {"value": self.earlier_than}))
 
-        if self.max and value >= self.max:
-            errors.append(Error("datetime.more_or_equal_then", {"max": self.max}))
+        if self.earlier_or_equal_to and value > self.earlier_or_equal_to:
+            errors.append(self.error("later_than", {"value": self.earlier_or_equal_to}))
+
+        if self.later_than and value <= self.later_than:
+            errors.append(self.error("earlier_or_equal_to", {"value": self.later_than}))
+
+        if self.later_or_equal_to and value < self.later_or_equal_to:
+            errors.append(self.error("earlier_than", {"value": self.later_or_equal_to}))
 
         return value, errors
 
-    def typecast(self, value):
-        if isinstance(value, datetime):
-            return value, []
+    def typecast(self, input):
+        if isinstance(input, datetime):
+            return input, []
 
-        if not isinstance(value, str):
-            return None, [Error("datetime.invalid_type_to_cast")]
+        if not isinstance(input, str):
+            return None, [self.error("unexpected_type", {"expected_type": "datetime"})]
 
         try:
             if self.format:
-                return datetime.strptime(value, self.format), []
+                return datetime.strptime(input, self.format), []
             else:
-                return datetime.fromisoformat(value), []
+                return datetime.fromisoformat(input), []
 
         except ValueError:
-            return None, [Error("datetime.invalid_format")]
+            return None, [self.error("invalid_datetime_format")]
 
 
 class Int(Schema):
--- tests/types/test_datetime.py
+++ tests/types/test_datetime.py
@@ -1,72 +1,152 @@
-from datetime import datetime, timedelta
+from datetime import datetime
+
+import pytest
 
 from goodboy.errors import Error
 from goodboy.types import DateTime
 from tests.types.conftest import assert_errors
 
 
-def test_min():
-    minimum = datetime(1970, 1, 1, 0, 0, 0)
-    schema = DateTime(min=minimum)
-
-    with assert_errors([Error("datetime.less_then", {"min": minimum})]):
-        schema(datetime(1900, 1, 1, 0, 0, 0))
-
-    assert schema(minimum) == minimum
+def test_accepts_none_when_none_allowed():
+    schema = DateTime(allow_none=True)
+    assert schema(None) is None
 
 
-def test_max():
-    maximum = datetime(1970, 1, 1, 0, 0, 0)
-    schema = DateTime(max=maximum)
-
-    with assert_errors([Error("datetime.more_or_equal_then", {"max": maximum})]):
-        schema(maximum)
-
-    assert schema(maximum - timedelta(seconds=1)) == maximum - timedelta(seconds=1)
-
-
-def test_typecheck():
-    good_value = datetime(2000, 1, 1, 0, 0, 0)
-    bad_value = "2000-01-01T00:00:00"
-
+def test_rejects_none_when_none_denied():
     schema = DateTime()
+
+    with assert_errors([Error("cannot_be_none")]):
+        schema(None)
+
+
+def test_accepts_datetime_type():
+    schema = DateTime()
+    good_value = datetime(2000, 1, 1, 0, 0, 0)
 
     assert schema(good_value) == good_value
 
-    with assert_errors([Error("invalid_type", {"expected_type": "datetime"})]):
+
+def test_rejects_non_datetime_type():
+    schema = DateTime()
+    bad_value = "2000-01-01T00:00:00"
+
+    with assert_errors([Error("unexpected_type", {"expected_type": "datetime"})]):
         schema(bad_value)
 
 
-def test_default_typecast():
-    value = datetime(2000, 1, 1, 0, 0, 0)
-
-    good_string = "2000-01-01T00:00:00"
-    bad_string = "2000/01/01 00:00:00"
-
+def test_type_casting_accepts_good_input_with_default_format():
     schema = DateTime()
-
-    assert schema(good_string, typecast=True) == value
-
-    with assert_errors([Error("datetime.invalid_format")]):
-        schema(bad_string, typecast=True)
-
-
-def test_format_typecast():
+    good_input = "2000-01-01T00:00:00"
     value = datetime(2000, 1, 1, 0, 0, 0)
 
-    good_string = "2000/01/01 00:00:00"
-    bad_string = "2000-01-01T00:00:00"
+    assert schema(good_input, typecast=True) == value
 
+
+def test_type_casting_rejects_bad_input_with_default_format():
+    schema = DateTime()
+    bad_input = "2000/01/01 00:00:00"
+
+    with assert_errors([Error("invalid_datetime_format")]):
+        schema(bad_input, typecast=True)
+
+
+def test_type_casting_accepts_good_input_with_custom_format():
     schema = DateTime(format="%Y/%m/%d %H:%M:%S")
+    good_input = "2000/01/01 00:00:00"
+    value = datetime(2000, 1, 1, 0, 0, 0)
 
-    assert schema(good_string, typecast=True) == value
-
-    with assert_errors([Error("datetime.invalid_format")]):
-        schema(bad_string, typecast=True)
+    assert schema(good_input, typecast=True) == value
 
 
-def test_allow_none():
-    assert DateTime(allow_none=True)(None) is None
+def test_type_casting_rejects_bad_input_with_custom_format():
+    schema = DateTime(format="%Y/%m/%d %H:%M:%S")
+    bad_input = "2000-01-01T00:00:00"
 
-    with assert_errors([Error("cannot_be_none")]):
-        DateTime()(None)
+    with assert_errors([Error("invalid_datetime_format")]):
+        schema(bad_input, typecast=True)
+
+
+def test_earlier_than_option_accepts_good_value():
+    schema = DateTime(earlier_than=datetime(1970, 1, 1, 0, 0, 0))
+    good_value = datetime(1900, 1, 1, 0, 0, 0)
+
+    assert schema(good_value) == good_value
+
+
+@pytest.mark.parametrize(
+    "bad_value",
+    [
+        datetime(1970, 1, 1, 0, 0, 0),
+        datetime(2000, 1, 1, 0, 0, 0),
+    ],
+)
+def test_earlier_than_option_rejects_bad_values(bad_value):
+    earlier_than_value = datetime(1970, 1, 1, 0, 0, 0)
+    schema = DateTime(earlier_than=earlier_than_value)
+
+    with assert_errors([Error("later_or_equal_to", {"value": earlier_than_value})]):
+        schema(bad_value)
+
+
+@pytest.mark.parametrize(
+    "good_value",
+    [
+        datetime(1900, 1, 1, 0, 0, 0),
+        datetime(1970, 1, 1, 0, 0, 0),
+    ],
+)
+def test_earlier_or_equal_to_option_accepts_good_values(good_value):
+    schema = DateTime(earlier_or_equal_to=datetime(1970, 1, 1, 0, 0, 0))
+    assert schema(good_value) == good_value
+
+
+def test_earlier_or_equal_to_option_rejects_bad_value():
+    earlier_or_equal_to_value = datetime(1970, 1, 1, 0, 0, 0)
+    schema = DateTime(earlier_or_equal_to=earlier_or_equal_to_value)
+    bad_value = datetime(1970, 1, 1, 0, 0, 1)
+
+    with assert_errors([Error("later_than", {"value": earlier_or_equal_to_value})]):
+        schema(bad_value)
+
+
+def test_later_than_option_accepts_good_value():
+    schema = DateTime(later_than=datetime(1970, 1, 1, 0, 0, 0))
+    good_value = datetime(2000, 1, 1, 0, 0, 0)
+
+    assert schema(good_value) == good_value
+
+
+@pytest.mark.parametrize(
+    "bad_value",
+    [
+        datetime(1970, 1, 1, 0, 0, 0),
+        datetime(1900, 1, 1, 0, 0, 0),
+    ],
+)
+def test_later_than_option_rejects_bad_values(bad_value):
+    later_than_value = datetime(1970, 1, 1, 0, 0, 0)
+    schema = DateTime(later_than=later_than_value)
+
+    with assert_errors([Error("earlier_or_equal_to", {"value": later_than_value})]):
+        schema(bad_value)
+
+
+@pytest.mark.parametrize(
+    "good_value",
+    [
+        datetime(2000, 1, 1, 0, 0, 0),
+        datetime(1970, 1, 1, 0, 0, 0),
+    ],
+)
+def test_later_or_equal_to_option_accepts_good_values(good_value):
+    schema = DateTime(later_or_equal_to=datetime(1970, 1, 1, 0, 0, 0))
+    assert schema(good_value) == good_value
+
+
+def test_later_or_equal_to_option_rejects_bad_value():
+    later_or_equal_to_value = datetime(1970, 1, 1, 0, 0, 0)
+    schema = DateTime(later_or_equal_to=later_or_equal_to_value)
+    bad_value = datetime(1900, 1, 1, 0, 0, 1)
+
+    with assert_errors([Error("earlier_than", {"value": later_or_equal_to_value})]):
+        schema(bad_value)
