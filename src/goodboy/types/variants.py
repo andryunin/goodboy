435a1b8fe7392188e1b74deebf6424e67c996dc6
Maxim Andryunin <maxim.andryunin@gmail.com>
2021-12-19 22:51:11 +0700

    feat: add AnyOf schema

---
+++ src/goodboy/types/variants.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+from goodboy.messages import DEFAULT_MESSAGES, MessageCollectionType
+from goodboy.schema import Rule, Schema, SchemaError
+
+
+class AnyOf(Schema):
+    """
+    Accept value matches any of specified schemas, taking into account ``allow_none``
+    option.
+
+    :param schemas: List of schemas to match
+    :param allow_none: If true, value is allowed to be ``None``.
+    :param messages: Override error messages.
+    :param rules: Custom validation rules.
+    """
+
+    def __init__(
+        self,
+        schemas: list[Schema],
+        *,
+        messages: MessageCollectionType = DEFAULT_MESSAGES,
+        rules: list[Rule] = [],
+    ):
+        super().__init__(messages=messages, rules=rules)
+        self.schemas = schemas
+
+    def __call__(self, value, *, typecast=False, context: dict = {}):
+        value, errors = self.validate(value, typecast, context)
+
+        if errors:
+            raise SchemaError(errors)
+
+        return value
+
+    def validate(self, value, typecast: bool, context: dict = {}):
+        schema_errors = {}
+        errors = []
+
+        for schema_index, schema in enumerate(self.schemas):
+            try:
+                value = schema(value)
+            except SchemaError as e:
+                schema_errors[schema_index] = e.errors
+            else:
+                break
+        else:
+            errors.append(self.error("no_variant_found", {"errors": schema_errors}))
+
+        value, rule_errors = self.call_rules(value, typecast, context)
+
+        return value, errors + rule_errors
+
+    def typecast(self, input, context: dict = {}):
+        raise NotImplementedError()
---
+++ tests/types/test_any_of.py
@@ -0,0 +1,42 @@
+import pytest
+
+from goodboy.errors import Error
+from goodboy.types.simple import Str
+from goodboy.types.variants import AnyOf
+from tests.types.conftest import assert_errors
+
+schemas = [Str(allowed=["foo"]), Str(allowed=["bar"])]
+
+
+def test_accept_value_valid_by_any_schema():
+    assert AnyOf(schemas)("foo") == "foo"
+    assert AnyOf(schemas)("bar") == "bar"
+
+
+expected_schema_errors = {
+    0: [Error("not_allowed", {"allowed": ["foo"]})],
+    1: [Error("not_allowed", {"allowed": ["bar"]})],
+}
+
+expected_anyof_error = Error("no_variant_found", {"errors": expected_schema_errors})
+
+
+def test_rejects_value_invalid_by_all_schemas():
+    schema = AnyOf(schemas)
+
+    with assert_errors([expected_anyof_error]):
+        schema("oops")
+
+
+expected_rule_error = Error("rule_error")
+
+
+def rule(self: Str, value, typecast: bool, context: dict):
+    return value, [expected_rule_error]
+
+
+def test_applies_rules_when_value_not_is_invalid():
+    schema = AnyOf(schemas, rules=[rule])
+
+    with assert_errors([expected_anyof_error, expected_rule_error]):
+        schema("oops")
