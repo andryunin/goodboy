7dd6d9f36a10b2f6b4f5e3ed52d5a444c5adbbf0
Maxim Andryunin <maxim.andryunin@gmail.com>
2022-03-07 21:08:48 +0700

    feat: export all schema types form root module

--- src/goodboy/__init__.py
+++ src/goodboy/__init__.py
@@ -1,14 +1,20 @@
 from goodboy.declarative import DeclarativeBuilder, SimpleDeclarativeSchemaFabric
 from goodboy.types.dates import Date, DateTime
 from goodboy.types.dicts import Dict, Key
+from goodboy.types.lists import List
 from goodboy.types.numeric import Float, Int
-from goodboy.types.simple import AnyValue, Str
+from goodboy.types.python import CallableValue
+from goodboy.types.simple import AnyValue, Bool, NoneValue, Str
+from goodboy.types.variants import AnyOf
 from goodboy.validator import Result, Validator, validate
 
 __version__ = "0.1.0"
 
 __all__ = [
+    "AnyOf",
     "AnyValue",
+    "Bool",
+    "CallableValue",
     "Date",
     "DateTime",
     "DeclarativeBuilder",
@@ -16,6 +22,8 @@ __all__ = [
     "Float",
     "Int",
     "Key",
+    "List",
+    "NoneValue",
     "Result",
     "SimpleDeclarativeSchemaFabric",
     "Str",
